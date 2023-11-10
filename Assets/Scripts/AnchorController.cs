9077c552b78170cc9e5c46183e26f78c69692ef2
/CODER <maapel1703@gmail.com>
2023-11-10 21:36:45 +0530

    Anchor add Working

--- Assets/Scripts/AnchorController.cs
+++ Assets/Scripts/AnchorController.cs
@@ -1,12 +1,28 @@
+using Google.XR.ARCoreExtensions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 
 public class AnchorController : MonoBehaviour
 {
+    public double Latitude;
 
+    /// <summary>Gets or sets the longitude of this anchor.</summary>
+    public double Longitude;
+    public ARAnchorManager _anchorManager = null;
+
+    private ARGeospatialAnchor anchor;
+   
+    private AnchorResolutionState _anchorResolution = AnchorResolutionState.NotStarted;
+    private enum AnchorResolutionState
+    {
+        NotStarted,
+        InProgress,
+        Complete
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +36,67 @@ public class AnchorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_anchorResolution == AnchorResolutionState.NotStarted)
+        {
+            AddGeoAnchorAtRuntime();
+        }
     }
+    private void AddGeoAnchorAtRuntime()
+    {
+
+        
+
+        if (_anchorManager == null)
+        {
+            string errorReason =
+                "The Session Origin's AnchorManager is null.";
+            Debug.LogError("Unable to place ARGeospatialCreatorAnchor " + name + ": " +
+                errorReason);
+            _anchorResolution = AnchorResolutionState.Complete;
+            return;
+        }
+        StartCoroutine(ResolveTerrainAnchor());
+    }
+
+    private void FinishAnchor(ARGeospatialAnchor resolvedAnchor)
+    {
+        if (resolvedAnchor == null)
+        {
+            // If we failed once, resolution is likley to keep failing. Don't retry endlessly.
+            Debug.LogError("Failed to make Geospatial Anchor for " + name);
+            _anchorResolution = AnchorResolutionState.Complete;
+            return;
+        }
+
+        // Maintain an association between the ARGeospatialCreatorAnchor and the resolved
+        // ARGeospatialAnchor by making the creator anchor a child of the runtime anchor.
+        // We zero out the pose & rotation on the creator anchor, since the runtime
+        // anchor will handle that from now on.
+        transform.position = new Vector3(0, 0, 0);
+        transform.rotation = Quaternion.identity;
+        transform.SetParent(resolvedAnchor.transform, false);
+
+        _anchorResolution = AnchorResolutionState.Complete;
+        Debug.Log("Geospatial Anchor resolved: " + name);
+    }
+    private IEnumerator ResolveTerrainAnchor()
+    {
+       
+        double altitudeAboveTerrain = 10;
+        ARGeospatialAnchor anchor = null;
+        ResolveAnchorOnTerrainPromise promise =
+            _anchorManager.ResolveAnchorOnTerrainAsync(
+                Latitude, Longitude, altitudeAboveTerrain, transform.rotation);
+
+        yield return promise;
+        var result = promise.Result;
+        if (result.TerrainAnchorState == TerrainAnchorState.Success)
+        {
+            anchor = result.Anchor;
+        }
+
+        FinishAnchor(anchor);
+        yield break;
+    }
+    
 }
--- Assets/Scripts/ScheduleHandle.cs
+++ Assets/Scripts/ScheduleHandle.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using UnityEditor.PackageManager;
+
 using UnityEngine;
 using System.Net.Http;
 using System;
 using System.Linq;
 using Google.XR.ARCoreExtensions.GeospatialCreator.Internal;
+using Google.XR.ARCoreExtensions;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 [Serializable]
 public class Event
@@ -26,10 +29,15 @@ public class ScheduleHandle : MonoBehaviour
 {   
    
     
-    public GameObject anchor;
+    public GameObject anchor_obj;
+    private GameObject obj;
+
     private static readonly HttpClient client = new HttpClient();
     private List<EventInfo> eventInfos = new List<EventInfo>();
     public GameObject eventInfoPrefab;
+    List<double[]> locations = new List<double[]>();
+    List<GameObject> anchors = new List<GameObject>();
+    [SerializeField]private ARAnchorManager _anchorManager;
 
     // Start is called before the first frame update
     async void  Start()
@@ -38,52 +46,61 @@ public class ScheduleHandle : MonoBehaviour
         var responseString = await client.GetStringAsync("https://maadhav17.pythonanywhere.com/get_events");
         Debug.Log(responseString);
         
-        List<Event> infos = new List<Event>();
+        List<Event> events = new List<Event>();
         foreach ( string item in responseString.Split("|")) { 
             var eve= JsonUtility.FromJson<Event>(item);
-            infos.Add(eve); 
+            events.Add(eve); 
         }
         //Debug.Log(events.eventList);
-        EventInfo eventInfo = new EventInfo(infos, eventInfoPrefab,this.transform,anchor);      
-    }
+        //EventInfo eventInfo = new EventInfo(infos, eventInfoPrefab,this.transform,anchor_obj);
 
-    // Update is called once per frame
-    void Update()
-    {
+        int i = 0;
         
-    }
-}
-public class EventInfo
-{
-    private GameObject obj;
-    public EventInfo(List<Event> events, GameObject eventInfoPrefab , Transform parent,GameObject anchor)
-    {
-        
-        int i =0;
-        List<double[]> locations = new List<double[]>();
-        List<GameObject> anchors = new List<GameObject>();
-        foreach (Event e in events) 
+        foreach (Event e in events)
         {
-              
-            double[] location = {double.Parse(e.latitude) , double.Parse(e.longitude) };
-            
-            if (!locations.Any(elem => elem.SequenceEqual(location))){
+
+            double[] location = { double.Parse(e.latitude), double.Parse(e.longitude) };
+
+            if (!locations.Any(elem => elem.SequenceEqual(location)))
+            {
                 locations.Add(location);
-                obj = GameObject.Instantiate(anchor, parent);
-                
+                obj = GameObject.Instantiate(anchor_obj, this.transform);
+
                 anchors.Add(obj);
-                ARGeospatialCreatorAnchor creatorAnchor = obj.GetComponent<ARGeospatialCreatorAnchor>();
-                creatorAnchor.Latitude = location[0] ;
-                creatorAnchor.Longitude = location[1];
+                AnchorController anchorController = obj.GetComponent<AnchorController>();
+
+                
+                var latitude = location[0];
+                var longitude = location[1];
+                anchorController.Latitude = latitude;
+                anchorController.Longitude = longitude;
+                anchorController._anchorManager = _anchorManager;
             }
             int ind = locations.FindIndex(elem => elem.SequenceEqual(location));
-            
+
             obj = GameObject.Instantiate(eventInfoPrefab, anchors[ind].transform);
             obj.GetComponent<EventController>().setParams(e);
-            obj.transform.parent.GetComponent<AnchorController>().arrange();
-            
+            anchors[ind].GetComponent<AnchorController>().arrange();
+
 
         }
     }
+    
+    
+    private void Update()
+    {
+        
+     
+        
+        
+        // Your code came here
+        
+
+    }
+
+    
+
+    // Update is called once per frame
 
 }
+
